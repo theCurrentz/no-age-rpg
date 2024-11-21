270f7fa2291748fd67df6500b01a9f11b84fd175
Izac Owen <owenizac@gmail.com>
2024-11-20 20:20:42 -0800

    AddedSpeed

--- src/main/java/player/PlayerComponent.java
+++ src/main/java/player/PlayerComponent.java
@@ -11,7 +11,7 @@ public class PlayerComponent extends Component {
 
 private TransformComponent position = new TransformComponent();
 
-//    private double speed = 0;\
+    private double speed;
     AnimatedTexture playerTexture;
     AnimationChannel idle;
 
@@ -27,35 +27,36 @@ private TransformComponent position = new TransformComponent();
         entity.getViewComponent().addChild(playerTexture);
         playerTexture.loopAnimationChannel(idle);
     }
-
+    //1 tick = 1/1,000,000 of a second
+    //TPF = Tick per frame
     @Override
     public void onUpdate(double tpf) {
-//        speed = tpf * 60;
+        speed = tpf * 60;
     }
 
     public void up() {
-        position.translateY(-5);
+        position.translateY(-speed);
         if (playerTexture.getAnimationChannel() != idle) {
             playerTexture.loopAnimationChannel(idle);
         }
     }
 
     public void down() {
-        position.translateY(5);
+        position.translateY(speed);
         if (playerTexture.getAnimationChannel() != idle) {
             playerTexture.loopAnimationChannel(idle);
         }
     }
 
     public void left() {
-        position.translateX(-5);
+        position.translateX(-speed);
         if (playerTexture.getAnimationChannel() != idle) {
             playerTexture.loopAnimationChannel(idle);
         }
     }
 
     public void right() {
-        position.translateX(5);
+        position.translateX(speed);
         if (playerTexture.getAnimationChannel() != idle) {
             playerTexture.loopAnimationChannel(idle);
         }
