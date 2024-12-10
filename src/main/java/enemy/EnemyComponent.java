727b35fcb0f272a597db81e65791d319eb434e66
Eric Suttle <ejsuttle88@gmail.com>
2024-12-09 21:12:40 -0800

    ES enemies Updated 12-9-2024--Needs help with enemy creation/images--need to update UI class

--- src/main/java/enemy/EnemyComponent.java
+++ src/main/java/enemy/EnemyComponent.java
@@ -3,34 +3,21 @@ package enemy;
 
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.component.Component;
-import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.texture.AnimatedTexture;
 import com.almasb.fxgl.texture.AnimationChannel;
 import javafx.util.Duration;
-import player.PlayerComponent;
-
-
-import com.almasb.fxgl.core.math.FXGLMath;
-import com.almasb.fxgl.entity.component.Component;
-import com.almasb.fxgl.physics.PhysicsComponent;
-import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.texture.AnimatedTexture;
-import com.almasb.fxgl.texture.AnimationChannel;
-import javafx.util.Duration;
-import java.util.Timer;
-import java.util.TimerTask;
 
 
 public class EnemyComponent extends Component {
 
     AnimatedTexture enemyTexture;
     AnimationChannel skelIdle;
-
+    public void onAdded(){
+        entity.getViewComponent().addChild(enemyTexture);
+    }
     public EnemyComponent(){
         skelIdle = new AnimationChannel(FXGL.image("enemy/Idle.png"), Duration.seconds(2), 4);
         enemyTexture = new AnimatedTexture(skelIdle);
-        enemyTexture.setTranslateX(-32);
-
         System.out.println("trigger EnemyComponent");
     }
 }
--- src/main/java/enemy/EnemyFactory.java
+++ src/main/java/enemy/EnemyFactory.java
@@ -22,9 +22,6 @@ public class EnemyFactory implements EntityFactory {
         ENEMY
     }
 
-
-
-
     @Spawns("Enemy")
     public Entity spawnEnemy(SpawnData data) {
         PhysicsComponent physics = new PhysicsComponent();
@@ -35,7 +32,6 @@ public class EnemyFactory implements EntityFactory {
                 .type(EnemyFactory.EntityType.ENEMY)
                 .at((double)FXGLMath.random(0, FXGL.getAppWidth() - 10) , (double)FXGLMath.random(0, FXGL.getAppHeight() - 300))
 
-//                .with(new CollidableComponent(true))
                 .with(physics)
 
                 .bbox(new HitBox(BoundingShape.box(40, 64)))
